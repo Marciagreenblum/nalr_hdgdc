a600b12f27a5e35e55539eef6b4e2163b3782117
David <david@cadeyrn.us>
2015-06-04 18:43:20 -0500

    Added breadcrumb override to prevent duplicate crumbs due to the Facet API.

--- template.php
+++ template.php
@@ -45,3 +45,67 @@ function nalr_hdgdc_preprocess_page(&$variables, $hook) {
   //dpm($variables['page']);
 
 }
+
+function nalr_hdgdc_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  $output = '';
+
+  // Determine if we are to display the breadcrumb.
+  $show_breadcrumb = theme_get_setting('zen_breadcrumb');
+  if ($show_breadcrumb == 'yes' || $show_breadcrumb == 'admin' && arg(0) == 'admin') {
+
+    // Optionally get rid of the homepage link.
+    $show_breadcrumb_home = theme_get_setting('zen_breadcrumb_home');
+    if (!$show_breadcrumb_home) {
+      array_shift($breadcrumb);
+    }
+
+    // Return the breadcrumb with separators.
+    if (!empty($breadcrumb)) {
+      $breadcrumb_separator = filter_xss_admin(theme_get_setting('zen_breadcrumb_separator'));
+      $trailing_separator = $title = '';
+      if (theme_get_setting('zen_breadcrumb_title')) {
+        $item = menu_get_item();
+        if (!empty($item['tab_parent'])) {
+          // If we are on a non-default tab, use the tab's title.
+          $breadcrumb[] = check_plain($item['title']);
+        }
+        else {
+          // Don't add the page title to the breadcrumb trail if the Facet API
+          // has already added it.
+          $page_title_exists = FALSE;
+          $page_title = drupal_get_title();
+          foreach ($breadcrumb as $key => $title) {
+            if (strip_tags($title) == $page_title) {
+              $page_title_exists = TRUE;
+            }
+          }
+          if (!$page_title_exists) {
+            $breadcrumb[] = $page_title;
+          }
+        }
+      }
+      elseif (theme_get_setting('zen_breadcrumb_trailing')) {
+        $trailing_separator = $breadcrumb_separator;
+      }
+
+      // Provide a navigational heading to give context for breadcrumb links to
+      // screen-reader users.
+      if (empty($variables['title'])) {
+        $variables['title'] = t('You are here');
+      }
+      // Unless overridden by a preprocess function, make the heading invisible.
+      if (!isset($variables['title_attributes_array']['class'])) {
+        $variables['title_attributes_array']['class'][] = 'element-invisible';
+      }
+
+      // Build the breadcrumb trail.
+      $output = '<nav class="breadcrumb" role="navigation">';
+      $output .= '<span' . drupal_attributes($variables['title_attributes_array']) . '>' . $variables['title'] . '</span>';
+      $output .= '<ol><li>' . implode($breadcrumb_separator . '</li><li>', $breadcrumb) . $trailing_separator . '</li></ol>';
+      $output .= '</nav>';
+    }
+  }
+
+  return $output;
+}
